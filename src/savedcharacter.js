1996dd69265a70033ade7b04e12655adc3b00a8d
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-09-13 15:22:57 +1000

    removed copy to clipboard function as redundant

--- src/savedcharacter.js
+++ src/savedcharacter.js
@@ -12,7 +12,6 @@ function displaySavedCharacterAsCard(characterData) {
             ${attributesWithoutReRoll}
         </div>
         <button class="deleteCharacterButton" data-id="${characterData.id}">Delete</button>
-        <button class="copyCharacterButton">Copy to Clipboard</button>
     `;
 
     card.querySelector('.deleteCharacterButton').addEventListener('click', () => {
@@ -20,9 +19,6 @@ function displaySavedCharacterAsCard(characterData) {
         card.remove();
     });
 
-    card.querySelector('.copyCharacterButton').addEventListener('click', () => {
-        copyToClipboard(characterData.attributes);
-    });
 
     savedCharacterList.appendChild(card);
 }
@@ -43,30 +39,6 @@ async function deleteCharacter(characterId) {
     }
 }
 
-function copyToClipboard(text) {
-    const textArea = document.createElement('textarea');
-    textArea.value = text;
-
-    textArea.style.position = 'absolute';
-    textArea.style.left = '-9999px';
-
-    document.body.appendChild(textArea);
-    textArea.select();
-
-    try {
-        const successful = document.execCommand('copy');
-        if (successful) {
-            console.log('Text copied to clipboard.');
-        } else {
-            console.error('Unable to copy text to clipboard.');
-        }
-    } catch (error) {
-        console.error('Error copying text to clipboard:', error);
-    } finally {
-        document.body.removeChild(textArea);
-    }
-}
-
 document.addEventListener('click', (event) => {
     const target = event.target;
 
@@ -78,8 +50,4 @@ document.addEventListener('click', (event) => {
         }
     }
 
-    if (target.classList.contains('copyCharacterButton')) {
-        const attributes = target.closest('.character-card').querySelector('.attributes').textContent;
-        copyToClipboard(attributes);
-    }
 });
